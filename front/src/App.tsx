1abf3257d78955f9d833ab586079ec639ce925b2
quorrra <rrotaruelena284@gmail.com>
2023-12-06 23:40:45 +0200

    A fost fun

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
-import './RegisterLogIn.css'
+import './components/RegisterLogIn.css'
 
 import { Route, Routes } from "react-router-dom";
 //import Home from './components/home';
 import 'bootstrap/dist/css/bootstrap-grid.min.css'
 import LoginRegister from './components/RegisterLogIn'; 
+import Navbar from './components/Navbar';
 
 function App() {
 
--- front/src/components/RegisterLogIn.tsx
+++ front/src/components/RegisterLogIn.tsx
@@ -1,11 +1,24 @@
 //import React from 'react';
 import './RegisterLogIn.css'
+import React, { useState } from 'react';
 
 const RegisterLogIn=()=>{
+ //   const[action, setAction]= useState("Create account");
+
+ const [action,setAction]=useState("Register");
+
+
+
   return(
+    <div className='navbar'>
+       <ul>
+        <li>Login</li>
+        <li>Register</li>
+        </ul>
+    
     <div className='container'>
        <div className='header'>
-        <div className='text'>Register</div>
+        <div className='text'>{action}</div>
          <div className='underline'></div>
        </div>
        <div className='inputs'>
@@ -18,7 +31,12 @@ const RegisterLogIn=()=>{
           <input type='password' placeholder='Password'/>
         </div>
        </div>
+       <div className='submit-container'>
+         <div className={action==='Login'? 'submit gray': 'submit' }>Create account</div>
+         <div className= {action==='Create account'?'submit gray': 'submit'}>Login</div>
+       </div>
        
+    </div>
     </div>
   )
 }
